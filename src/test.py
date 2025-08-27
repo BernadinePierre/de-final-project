e4e808a0854d840371f9d1c9e6e0ad9a172449b3
bramhambutts <szbbutts@gmail.com>
2025-08-27 15:14:22 +0100

    Connect to the S3 bucket and add files for collected data

--- src/test.py
+++ src/test.py
@@ -44,8 +44,22 @@ def connect_to_original_database():
         print(f'Database connection failed due to {e}')
 
 
-def check_original_updates():
+def check_original_update(table_name):
     connection = connect_to_original_database()
+    query = f'SELECT last_updated FROM {table_name} ORDER BY last_updated DESC LIMIT 1'
+    connection.execute(query)
+    return connection.fetchall()[0][0].strftime(r'%Y-%m-%d')
+    # print(f'Table <{table}> was last updated at:\n{result[0][0].strftime(r'%Y-%m-%d')}')
+
+
+def get_original_updates(table_name):
+    connection = connect_to_original_database()
+    query = f'SELECT * FROM {table_name} LIMIT 5'
+    connection.execute(query)
+    return [col[0] for col in connection.description], connection.fetchall()
+
+
+def connect_to_s3():
     table_list = [
         'address',
         'counterparty',
@@ -59,11 +73,21 @@ def check_original_updates():
         'staff',
         'transaction'
     ]
+    s3_client = boto3.client('s3')
     for table in table_list:
-        query = f'SELECT last_updated FROM {table} ORDER BY last_updated DESC LIMIT 1'
-        connection.execute(query)
-        result = connection.fetchall()
-        print(f'Table <{table}> was last updated at:\n{result[0][0].strftime(r'%Y-%m-%d')}')
+        date = check_original_update(table)
+        # TODO
+        # check here whether we need to update or can move on
+        # or create a minimised list of tables to loop over before
+
+        columns, data = get_original_updates(table)
+        data = pd.DataFrame(data, columns=columns)
+        s3_client.put_object(
+            Bucket='nc-crigglestone-ingest-bucket',
+            Key=f'{table}/{date}.csv',
+            Body=data.to_csv(index=False)
+        )
+        print(f'Table {table} updated into S3')
 
 
 def lambda_handler(event, context):
@@ -71,4 +95,4 @@ def lambda_handler(event, context):
 
 
 if __name__ == '__main__':
-    check_original_updates()
+    connect_to_s3()
